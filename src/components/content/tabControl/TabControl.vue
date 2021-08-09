48bfe36e3e51621678e7cc16c03c11d3bd16a5c9
codeMao <1643408098@qq.com>
2021-08-09 17:37:51 +0800

    一次修改

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="tab-control">
+    <div v-for="(item,index) in titles" 
+    class="tab-control-item":class="{active:index===currentIndex}" @click="btnclick(index)">
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+name:'TabControl',
+props:{
+  titles:{
+    type:Array,
+    default(){
+      return []
+    }
+  }
+},
+data(){
+  return {
+    currentIndex:0
+  }
+},
+methods:{
+  btnclick(index){
+    this.currentIndex =index;
+  }
+}
+}
+</script>
+
+<style>
+.tab-control{
+  display: flex;
+  text-align: center;
+  font-size: 15px;
+  background: #fff;
+}
+.tab-control-item{
+  height: 40px;
+  line-height: 40px;
+  flex: 1;
+}
+.tab-control-item span{
+  padding: 5px;
+}
+.active{
+  color:var(--color-high-text);
+}
+.active span{
+  border-bottom:3px solid var(--color-tint);
+
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,20 +3,132 @@
 <nav-bar class="home-nav"><div slot='center'>购物街</div></nav-bar>
 <home-swipe :banner="banner"></home-swipe>
 <recommend-view :recommends="recommends"></recommend-view>
+<feature-view></feature-view>
+<tab-control :titles="['流行','新款','精选']" class="tab-control"></tab-control>
+<ul>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+</ul>
 </div>
 </template>
 
 <script>
-import NavBar from '../../components/common/navbar/NavBar.vue'
-import { getHomeMultidata } from "../../network/home";
+//子组件
 import HomeSwipe from './homeCoplate/HomeSwipe.vue'
 import RecommendView from './homeCoplate/RecommendView.vue'
+import FeatureView from './homeCoplate/FeatureView.vue'
+//公共组件
+import NavBar from '../../components/common/navbar/NavBar.vue'
+import TabControl from '../../components/content/tabControl/TabControl.vue'
+//发送请求
+import { getHomeMultidata } from "../../network/home";
+
 export default {
 name:'Home',
 components:{
-  NavBar,
   HomeSwipe,
-  RecommendView
+  RecommendView,
+  FeatureView,
+  TabControl,
+  NavBar,
 },
 data(){
   return {
@@ -34,8 +146,20 @@ created(){
 </script>
 
 <style>
+#home{
+  padding-top:44px;
+}
 .home-nav{
   background: var(--color-tint);
-  color:#fff
+  color:#fff;
+  position: fixed;
+  left:0;
+  top:0px;
+  right: 0;
+  z-index: 5;
+}
+.tab-control{
+  position: sticky;
+  top:44px;
 }
 </style>
